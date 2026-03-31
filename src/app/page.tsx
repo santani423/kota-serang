4bcd5fa94f410368c12e247e66702b6aa0bb2ca9
santani423 <santani423@gmail.com>
2026-03-31 09:29:30 +0700

    update 31 03 26

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Template from "@/components/layout/template";
-import HeroSection from "@/components/layout/hompage/HeroSaction";
+import HeroSection from "@/components/layout/hompage/HeroSection";
 import QuickServicesSection from "@/components/layout/hompage/QuickServicesSection";
 import NewsPreviewSection from "@/components/layout/hompage/NewsPreviewSection";
 import StatisticsSection from "@/components/layout/hompage/StatisticsSection";
