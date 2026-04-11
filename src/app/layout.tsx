2ddafdbcff8f86d9789f7be457fc6116ae3f877f
santani423 <santani423@gmail.com>
2026-04-12 01:03:08 +0700

    penyesuaian

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,23 @@ export default function RootLayout({
     >
       <head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        {/* Matomo Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _paq = window._paq = window._paq || [];
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="//matomo-cms-kota-serang.santani.dev/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            `,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col">
         <ReduxProvider>{children}</ReduxProvider>
--- src/components/layout/template.tsx
+++ src/components/layout/template.tsx
@@ -10,28 +10,41 @@ import { fetchSettingsCorMenu } from "@/lib/services/settingsServices";
 import { setMenu } from "@/store/settingsSlice";
 import { Button } from "../ui/button";
 import { Metadata } from "next";
- 
+import { postSupport } from "@/lib/services/settingsServices";
+
 export default function Template({ children }: { children: React.ReactNode }) {
-  const dispatch = useAppDispatch(); 
+  const dispatch = useAppDispatch();
   useEffect(() => {
     const getData = async () => {
       try {
-        const res = await fetchSettingsCorMenu(); 
+        const res = await fetchSettingsCorMenu();
         dispatch(setMenu(res.data));
       } catch (err) {
         console.error(err);
       }
-    }; 
+    };
     getData();
+    postSupport({
+      email: "serang kota vewbsite",
+      hp: Date.now().toString(),
+      message: new Date().toLocaleString("id-ID"),
+    });
   }, []);
   return (
     <>
       <Header />
-      <main className="min-h-screen bg-serang-bg dark:bg-serang-bg-dark">{children}</main>
+      <main className="min-h-screen bg-serang-bg dark:bg-serang-bg-dark">
+        {children}
+      </main>
       <AccessibilityComponent />
       <Suport />
       <Footer />
-       <div className="fixed bottom-4 left-20 z-50"><Button variant={"outline"}> <span className="gradient-title"> Web Konsep </span> </Button></div>
+      <div className="fixed bottom-4 left-20 z-50">
+        <Button variant={"outline"}>
+          {" "}
+          <span className="gradient-title"> Web Konsep </span>{" "}
+        </Button>
+      </div>
     </>
   );
 }
